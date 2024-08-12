ca76f4ffac75f7eee514e972c30f6b9f12d52c47
Tayyab Abdullah <tabdullah0801@gmail.com>
2024-08-12 10:59:34 +0500

    Added code for authentication of users

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const {User} = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+function validate(req) {
+    const schema = {
+      email: Joi.string().min(5).max(255).email().required(),
+      password: Joi.string().min(5).max(255).required()
+    };
+  
+    return Joi.validate(req, schema);
+  }
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Invalid email and password....');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email and password....');
+
+    res.send(true);
+
+});
+
+
+module.exports = router;
